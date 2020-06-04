09ac24a6d876057772b29d36478ce615f424799f
vamc554 <vamc1994@gmail.com>
2020-06-04 22:28:50 +0530

    tags added

--- db.js
+++
@@ -1,3 +0,0 @@
-var mongoose=require('mongoose');
-var connection=mongoose.connect('mongodb://localhost/OmniwyseProject', {useNewUrlParser: true, useUnifiedTopology: true});
-module.exports=connection;
